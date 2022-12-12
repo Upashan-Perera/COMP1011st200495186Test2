f16cb749915320640aae92f0ea2e642612f8bc38
Upashan-Perera <96879265+Upashan-Perera@users.noreply.github.com>
2022-12-12 11:33:14 -0500

    Question 1a complete

---
+++ src/main/java/com/example/comp1011st200495186test2/Song.java
@@ -0,0 +1,59 @@
+package com.example.comp1011st200495186test2;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Song {
+
+    @SerializedName("SongTitle")
+    private String songTitle;
+
+    @SerializedName("performer")
+    private String performer;
+
+    @SerializedName("weekPosition")
+    private int weekPosition;
+
+    @SerializedName("Year")
+    private int year;
+
+    @SerializedName("month")
+    private int month;
+
+    @SerializedName("day")
+    private int day;
+
+    @SerializedName("url")
+    private String url;
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public String getPerformer() {
+        return performer;
+    }
+
+    public int getWeekPosition() {
+        return weekPosition;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String toString() {
+        return songTitle + " - " + performer + " - (" + year + "/" + month + "/" + day + ")";
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.comp1011st200495186test2 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
-    opens com.example.comp1011st200495186test2 to javafx.fxml;
+    requires java.net.http;
+    requires com.google.gson;
+
+
+    opens com.example.comp1011st200495186test2 to javafx.fxml,com.google.gson;
     exports com.example.comp1011st200495186test2;
 }
